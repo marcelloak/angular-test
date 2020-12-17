df5659f18001063735a3b074721d17cd6e7949b0
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-12-16 17:11:13 -0700

    Moved artist info into component

--- src/app/artist-items/artist-items.component.ts
+++ src/app/artist-items/artist-items.component.ts
@@ -3,9 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-artist-items',
   templateUrl: './artist-items.component.html',
-  styleUrls: ['./artist-items.component.sass']
+  inputs: ['artist']
 })
 export class ArtistItemsComponent implements OnInit {
+  artist = {
+    name: String,
+    shortname: String,
+    reknown :String,
+    bio :String
+  }
 
   constructor() { }
 
